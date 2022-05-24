f160711b23dcfef22be89465244f5d80526c8fff
godgiev <godgiev@tjcollection.ru>
2022-05-24 16:00:16 +0300

    first commit

---
+++ pages/fruits.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Fruits</h1>
+</template>
+
+<script>
+export default {
+  name: 'FruitPage',
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+      {{ name }} <br>
+  <nuxt-link to="/fruits">Фрукты</nuxt-link> <br>
+    <div v-for="(fruit, key) in fruits" :key="key">
+      <h2>{{ fruit.title }}</h2>
+      <p>{{ fruit.price }}</p>
+      <button @click="addToCart(fruit)">Добавить</button>
+    </div>
+    <div>
+      Корзина: {{ basket }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data(){
+    return {
+      name: 'Tamik',
+      fruits: [
+        {
+          title: 'Яблоко',
+          price: 232,
+        },
+                {
+          title: 'Груша',
+          price: 232,
+        },
+                {
+          title: 'Киви',
+          price: 232,
+        },
+      ],
+      basket: []
+    }
+  },
+  methods: {
+    addToCart(obj) {
+      this.basket.push(obj)
+      console.log('added')
+    }
+  }
+}
+</script>
